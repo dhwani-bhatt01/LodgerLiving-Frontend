2050ae9464f584918376fb1b694bf1448f739be5
dhwani bhatt <bhattdhwani01@gmail.com>
2022-12-04 23:32:38 +0530

    made some changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-	baseURL: "http://localhost:8000/api",
+	// baseURL: "http://localhost:8000/api",
+	baseURL: "https://lodger-living-backend.vercel.app/api",
 });
 //axios fro fetching data and making http requests
